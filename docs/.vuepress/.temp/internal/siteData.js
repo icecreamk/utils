f22abb3f094a20ac19b1acb49b4c69129dfcb2d6
fflinkexin <fflinkexin@chinatelecom.cn>
2024-08-08 17:36:19 +0800

    feat: build docs

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"fun-utils\",\"description\":\"first vuepress\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/utils/\",\"lang\":\"zh-CN\",\"title\":\"fun-utils\",\"description\":\"first vuepress\",\"head\":[],\"locales\":{}}")
